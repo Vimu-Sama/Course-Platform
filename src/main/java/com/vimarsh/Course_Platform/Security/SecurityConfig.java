9838177e38394f7e600d5ce6d0de0731a615a1b2
Vimarsh Sharma <vimarshsharmabsr2000@gmail.com>
2026-02-04 22:35:24 +0530

    Security Config added and pom.xml updated to add spring-boot-starter-security

---
+++ src/main/java/com/vimarsh/Course_Platform/Security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.vimarsh.Course_Platform.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/register",
+                                "/api/auth/login",
+                                "/api/search/**",
+                                "/api/courses/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
